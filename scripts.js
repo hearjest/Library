9dd404036c5833498edd29f7c56b63f9bc644d55
Brian <bzengprsnl@gmail.com>
2021-08-17 13:51:21 -0400

    Basic functionality

--- scripts.js
+++ scripts.js
@@ -1,18 +1,42 @@
+const submitButton=document.getElementById('submit');
+submitButton.addEventListener('click',function(){
+    let title=document.getElementById('Title').textContent;
+    let author=document.getElementById('Author').textContent;
+    let pages=document.getElementById('Pages').textContent;
+    let readStatus=document.getElementById('ReadOrNot').value;
+    addBookToLibrary(title,author,pages,readStatus);
+});
+const displayButton=document.getElementById('displayBooks');
+displayButton.addEventListener('click',displayBooks());
+let myLibrary=[];
+
 function Book(title, author, pages,read){
     this.title=title;
     this.author=author;
     this.page=pages;
-    isRead=read;
-    this.info=function(){
-        let status;
-        if(isRead){
-            status='finished reading'
-        }else{
-            status='not read'
-        }
-        return `${title} by ${author}, ${pages} pages, ${status}`;
-    }; 
+    isRead=read; 
 }
 
-const theHobbit=new Book('The Hobbit','J.R.R Tolkein',295,false);
-console.log(theHobbit.info());
+Book.prototype.info=function(){
+    let status;
+    if(isRead){
+        status='finished reading'
+    }else{
+        status='not read'
+    }
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.status}`;
+};
+
+function addBookToLibrary(title,author,pages,readStatus){
+    myLibrary.push(new Book(title,author,pages,readStatus));
+};
+
+function displayBooks(){
+    let div;
+    let title;
+    let author;
+    let pages;
+    
+    for(let i=0;i<myLibrary.length;i++){
+    }
+}

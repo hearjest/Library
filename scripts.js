3cc232c5169aa73e2ad0843df14acd6b4bf56c4b
Brian <bzengprsnl@gmail.com>
2021-08-18 14:04:19 -0400

    Added remove button functionality

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+let index=0;
 const submitButton=document.getElementById('submitButton');
 submitButton.addEventListener('click',function(){
     let title=document.getElementById('BookTitle').value;
@@ -39,6 +40,12 @@ function displayBooks(){
     let authorBox=document.createElement('div');
     let pagesBox=document.createElement('div');
     let readBox=document.createElement('div');
+    let deleteButton=document.createElement('button');
+    deleteButton.setAttribute('type','submit');
+    deleteButton.textContent='Remove';
+    deleteButton.addEventListener('click',function(){
+        deleteButton.parentElement.remove();
+    })
     titleBox.textContent=myLibrary[myLibrary.length-1].title;
     authorBox.textContent=myLibrary[myLibrary.length-1].author;
     pagesBox.textContent=myLibrary[myLibrary.length-1].page;
@@ -47,10 +54,12 @@ function displayBooks(){
     }else{
         readBox.textContent='not read'
     }
+    div.setAttribute('id',`${index++}`);
     document.querySelector('#libraryContainer').appendChild(div);
     let thing=document.querySelectorAll('.bookCard')[document.querySelectorAll('.bookCard').length-1];
     thing.appendChild(titleBox);
     thing.appendChild(authorBox);
     thing.appendChild(pagesBox);
     thing.appendChild(readBox);
+    thing.appendChild(deleteButton);
 }
